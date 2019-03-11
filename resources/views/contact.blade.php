24166abeb93e2d304b6b4b9f092b06ebfde79de5
RindangRamadhan <rindangramadhan10@gmail.com>
2019-03-11 12:15:44 +0700

    Contact View

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,6 +1,28 @@
-@extends('layouts.app-about')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
+<head>
+    <title>PT Andall Hasa Prima</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+    <link rel="icon" href="../assets/img/favicon.png">
+    <link rel="stylesheet" href="../assets/libraries/bootstrap/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="../assets/libraries/font-awesome/css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="../assets/libraries/css/animate.min.css" type="text/css">
+    <link rel="stylesheet" href="../assets/libraries/slick/slick.css" type="text/css">
+    <link rel="stylesheet" href="../assets/css/variables.css" type="text/css">
+    <link rel="stylesheet" href="../assets/css/navbar.css" type="text/css">
+    <link rel="stylesheet" href="../assets/css/main.scss" type="text/css">
+    <link rel="stylesheet" href="../assets/css/header.css" type="text/css">
+    <link rel="stylesheet" href="../assets/css/footer.css" type="text/css">
+    <link rel="stylesheet" href="../assets/css/home.css" type="text/css">
+    <link rel="stylesheet" href="../assets/css/our-values.css" type="text/css">
+    <link rel="stylesheet" href="../assets/css/contact.css" type="text/css">
+    <!-- <link rel="stylesheet" href="../assets/css/style.css" type="text/css"> -->
+    <script src="../assets/libraries/js/jquery-3.3.1.min.js"></script>
+</head>
+
+<body>
     <div class="site-overlay"></div>
     <div id="wrapper">
 	    @extends('layouts.header')
@@ -117,4 +139,18 @@
             </div>
         </div>
     </div>
-@endsection
+    
+    @extends('layouts.footer')
+
+    <div class="scroll-up">
+        <a href="#wrapper"><i class="fa fa-angle-up"></i></a>
+    </div>
+    <script src="../assets/libraries/js/jquery.easing.min.js"></script>
+    <script src="../assets/libraries/bootstrap/js/bootstrap.min.js"></script>
+    <script src="../assets/libraries/pushy/js/pushy.min.js"></script>
+    <script src="../assets/libraries/slick/slick.min.js"></script>
+    <!-- <script src="../assets/libraries/jquery-mobile-custom/jquery.mobile.custom.min.js"></script> -->
+    <script src="../assets/js/main.js"></script>
+</body>
+
+</html>
